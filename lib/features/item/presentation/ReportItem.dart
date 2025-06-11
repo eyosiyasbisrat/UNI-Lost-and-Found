61eb3828a4a99ee12f2c967c098f1716fdf0331b
astroker00 <astroker920@gmail.com>
2025-06-11 22:38:36 +0300

    final setup for data and item file

--- lib/features/item/presentation/ReportItem.dart
+++ lib/features/item/presentation/ReportItem.dart
@@ -8,4 +8,26 @@ import '../../../../widgets/custom_bottom_nav_bar.dart';
 class ReportItemFoundScreen extends ConsumerWidget {
   const ReportItemFoundScreen({super.key});
 
-  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final reportState = ref.watch(reportControllerProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF27649A),
+        centerTitle: true,
+        title: const Text(
+          'REPORT ITEM',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0,
+      ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: ReportForm(),
+      ),
+      bottomNavigationBar: const CustomBottomNavBar(currentIndex: 2),
+    );
+  }
+}
