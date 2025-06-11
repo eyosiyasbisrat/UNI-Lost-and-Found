ea3f8791a63ba8d36c4c16e34bc12352698d5498
astroker00 <astroker920@gmail.com>
2025-06-11 22:37:47 +0300

    item domain and providers

---
+++ lib/features/item/presentation/ReportItem.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uni_lost_and_found/features/item/controllers/report_controller.dart';
+import 'package:uni_lost_and_found/features/item/widgets/report_form.dart';
+import '../../../../widgets/custom_bottom_nav_bar.dart';
+
+class ReportItemFoundScreen extends ConsumerWidget {
+  const ReportItemFoundScreen({super.key});
+
+  
