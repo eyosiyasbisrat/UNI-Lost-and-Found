6f0e68c77553c7d26636ee98e21257ce2db0699b
astroker00 <fikertilahun381@gmail.com>
2025-06-11 22:52:01 +0300

    message configuration

---
+++ lib/features/chat/presentation/ChatList.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../../../core/widgets/custom_bottom_nav.dart';
+import '../models/message.dart';
+import '../widgets/message_list_tile.dart';
+
+class MessagesScreen extends StatelessWidget {
+  const MessagesScreen({super.key});
+
+ 
